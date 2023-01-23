9ef9465ad021e59081436be8259338553ed3fa54
wongpakho814 <wongpakho814@gmail.com>
2023-01-23 15:47:46 +0800

    Implemented server/resolvers.js and typeDefs.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,47 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username }, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      return User.findOne({ username });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { username, email, password }) => {
+      const user = await User.findOne({
+        $or: [{ username: username }, { email: email }],
+      });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    }
+  },
+};
+
+module.exports = resolvers;
