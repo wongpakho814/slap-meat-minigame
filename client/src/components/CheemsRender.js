1065c5e9cbea2490c3cf8ecdfeb5e828d7e619b0
wongpakho814 <wongpakho814@gmail.com>
2023-01-30 18:58:10 +0800

    Implemented the timer in CheemsRender

--- client/src/components/CheemsRender.js
+++ client/src/components/CheemsRender.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import useState from "react-usestateref";
 import cheems10 from "../images/pet-animation-10.gif";
 import cheemsStatic from "../images/pet-animation-static.png";
 
@@ -10,21 +11,23 @@ import Auth from "../utils/auth";
 
 const CheemsRender = () => {
   const [playGif, setPlayGif] = useState(false);
-  const [seconds, setSeconds] = useState(0);
-  const { loading, error, data } = useQuery(GET_ME);
+  const [seconds, setSeconds, secondsRef] = useState(0);
+  const [ready, setReady] = useState(false);
+  const { error, data } = useQuery(GET_ME);
   const [updateTimePetted] = useMutation(UPDATE_TIME_PETTED);
 
   // get the user's accumulated timePetted here
   useEffect(() => {
     if (data) {
       setSeconds(data.me.timePetted);
+      setReady(true);
     }
-  }, [data]);
+  }, [data, setSeconds]);
 
   const startTimer = () => {
     setInterval(() => {
       setSeconds((seconds) => seconds + 1);
-      updateTimePetted({ variables: { timePetted: seconds } });
+      updateTimePetted({ variables: { timePetted: secondsRef.current } });
     }, 1000);
   };
 
@@ -38,16 +41,28 @@ const CheemsRender = () => {
 
   return (
     <>
-      {Auth.loggedIn() && (
-        error ? (
-            <span id="counter">Something went wrong when trying to connect to the Database!</span>
-          ) : (
-            <span id="counter">You have petted Cheems for {seconds}s in total! Hope it'll get happy soon :D</span>
-        )
-      )}
+      {Auth.loggedIn() &&
+        (error ? (
+          <span id="counter">
+            Something went wrong when trying to connect to the Database!
+          </span>
+        ) : (
+          ready && (
+            <span id="counter">
+              You have petted Cheems for {seconds}s in total! Hope it'll get
+              happy soon :D
+            </span>
+          )
+        ))}
       <div>
-        <span id="press-image-span">Press the image to start petting Cheems!</span>
-        <img alt="cheems pic" src={playGif ? cheems10 : cheemsStatic} onClick={() => onClick()} />
+        <span id="press-image-span">
+          Press the image to start petting Cheems!
+        </span>
+        <img
+          alt="cheems pic"
+          src={playGif ? cheems10 : cheemsStatic}
+          onClick={() => onClick()}
+        />
       </div>
     </>
   );
