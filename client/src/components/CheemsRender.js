1c52f6b97252aba737ea6db3f8942d812c3739d0
wongpakho814 <wongpakho814@gmail.com>
2023-01-30 16:44:16 +0800

    Implemented timer functionality to rendering cheems

--- client/src/components/CheemsRender.js
+++ client/src/components/CheemsRender.js
@@ -4,15 +4,33 @@ import cheemsStatic from "../images/pet-animation-static.png";
 
 function CheemsRender() {
   const [playGif, setPlayGif] = useState(false);
+  const [seconds, setSeconds] = useState(0);  
+
+  const startTimer = () => {
+    setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+  };
+
+  const onClick = () => {
+    if (playGif === false) {
+      startTimer();
+      document.querySelector("#press-image-span").remove();
+    }
+    setPlayGif(true);
+  };
   return (
-    <div>
-      <span>Press the image to start petting Cheems!</span>
-      <img
-        alt="cheems pic"
-        src={playGif ? cheems10 : cheemsStatic}
-        onClick={() => setPlayGif((prevMode) => true)}
-      />
-    </div>
+    <>
+      <span id="counter">You have petted Cheems for {seconds}s in total! Hope it'll get happy soon :D</span>
+      <div>
+        <span id="press-image-span">Press the image to start petting Cheems!</span>
+        <img
+          alt="cheems pic"
+          src={playGif ? cheems10 : cheemsStatic}
+          onClick={() => onClick()}
+        />
+      </div>
+    </>
   );
 }
 
