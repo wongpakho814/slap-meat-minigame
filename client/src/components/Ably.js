6ac322daf341537534d27cbb32506db80855c847
wongpakho814 <wongpakho814@gmail.com>
2023-02-02 21:32:16 +0800

    deploying -- attempting to fix the API key problem

--- client/src/components/Ably.js
+++ client/src/components/Ably.js
@@ -1,7 +1,3 @@
 import { Realtime } from "ably";
 
-console.log(
-  process.env.REACT_APP_ABLY_API_KEY,
-  String(process.env.REACT_APP_ABLY_API_KEY)
-);
-export default new Realtime(String(process.env.REACT_APP_ABLY_API_KEY));
+export default new Realtime(process.env.REACT_APP_ABLY_API_KEY);
