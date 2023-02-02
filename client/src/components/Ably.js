34e317c8fe10885aa7b1d1aabb9b7c4434ea6fdf
wongpakho814 <wongpakho814@gmail.com>
2023-02-02 19:10:32 +0800

    deploying

--- client/src/components/Ably.js
+++ client/src/components/Ably.js
@@ -1,3 +1,3 @@
 import { Realtime } from "ably";
 
-export default new Realtime(process.env.REACT_APP_ABLY_API_KEY);
+export default new Realtime({ keyOrToken: process.env.REACT_APP_ABLY_API_KEY });
